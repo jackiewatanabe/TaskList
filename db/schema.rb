cea313c6513d4250bb49591eec01ecd622066b57
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-24 15:00:47 -0700

    created new migration to add 'complete' column with boolean data type to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321204427) do
+ActiveRecord::Schema.define(version: 20170324214816) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170321204427) do
     t.date     "completion_date"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "complete"
   end
 
 end
