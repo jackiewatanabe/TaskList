f7d4cb6f193e11b466c14bc8ac706c5007efeb9f
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-24 14:15:02 -0700

    changed route prefix names to make them consistent

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
 
   root 'task#index'
 
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
   post 'tasks', to: 'tasks#create'
 
   get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
-  get 'tasks/edit/:id', to: 'tasks#edit', as: 'task_edit'
+  get 'tasks/edit/:id', to: 'tasks#edit', as: 'edit_task'
   put 'tasks/:id', to: 'tasks#update'
 
   delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
